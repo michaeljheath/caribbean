fc36e57a483184b8700e5cc53073aa973f23cddc
Michael Heath <m.heath66@gmail.com>
2017-05-04 08:58:45 -0300

    pick up related image from CMS

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -5,4 +5,20 @@ class Accommodation < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  has_attached_file :photo,
+                    :path => "#{ENV['CMS_ROOT']}/public/photo_attachment/:id/:style/:basename.:extension",
+                    :url => "#{ENV['CMS_ROOT']}/photo_attachment/:id/:style/:basename.:extension",
+                    styles: { thumb: ["300x300#", :jpg] }
+
+  validates_attachment :photo,
+                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+  def photo_path_thumbnail
+     #return ENV['PHOTO_DIR'] + "/" + self.id.to_s + '/thumb/'
+  end
+
+  def photo_path_original
+    #return ENV['PHOTO_DIR'] + "/" + self.id.to_s + '/original/'
+  end
 end
